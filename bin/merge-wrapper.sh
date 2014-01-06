1c11d71e7fd575d880cb545d5029ea7b71c7eee5
Xudong Huang <xudongh@juniper.net>
2014-01-06 10:39:06 +0800

    update from work

--- bin/merge-wrapper.sh
+++ bin/merge-wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # merge is called by git with 7 parameters:
 # path old-file old-hex old-mode new-file new-hex new-mode
-/home/dmwp37/bin/bdiff %1 %2 %3 %4 /title1="local" /title2="remote" /title3="base" /title4="$PWD/$MERGED" 
+~/bin/bdiff %1 %2 %3 %4 /title1="local" /title2="remote" /title3="base" /title4="$PWD/$MERGED" 
